d712e8d897dd98de0b22ac119d7d8f1959cbe42f
Benjamin Touchard <benjamin.touchard@butterfly-tx.com>
2024-09-27 14:22:44 +0200

    feature: add scale argument to sql def

--- sql/pgqrcode--1.0.0.sql
+++ sql/pgqrcode--1.0.0.sql
@@ -1,3 +1,4 @@
-CREATE FUNCTION generate_qr_code_svg(text) RETURNS text
+CREATE OR REPLACE FUNCTION generate_qr_code_svg(input TEXT, scale FLOAT8)
+RETURNS TEXT
 AS 'MODULE_PATHNAME', 'generate_qr_code_svg'
 LANGUAGE C STRICT;
